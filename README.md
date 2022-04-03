d6a2352c92b5526dcabe4ae12a32cfe584a752db
JRed <joseph@titaniumcoast.com>
2022-04-03 18:42:07 -0400

    Got It Working

---
+++ README.md
@@ -0,0 +1,15 @@
+# Atlas Splitter
+
+This program splits TextureAtlas xml files into multiple images used by many game engines and art/animation tools.
+
+## WARNING
+
+Does not yet support frames. Why? Cause I can't figure out how to make that work with the separated images.
+
+### How to install
+
+With NPM:
+`npm install -g atlas-splitter`
+
+With Yarn:
+`yarn global add atlas-splitter`
---
+++ bin/atlas-splitter.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+const { XMLParser } = require("fast-xml-parser");
+const fs = require("fs-extra");
+const Jimp = require("jimp");
+
+const { join, parse } = require("path");
+
+const parser = new XMLParser({
+    ignoreAttributes: false,
+    attributeNamePrefix: "$",
+});
+
+program
+    .name("atlas-splitter")
+    .description("Splits TextureAtlas XML Files")
+    .version("1.0.0")
+    .argument("<TextureAtlas>", "the path to the TextureAtlas to split")
+    .action(async (TextureAtlasPath) => {
+        const TextureAtlasData = fs.readFileSync(TextureAtlasPath);
+        const TextureAtlas = parser.parse(TextureAtlasData).TextureAtlas;
+
+        const imagePath = join(
+            TextureAtlasPath,
+            "../",
+            TextureAtlas.$imagePath
+        );
+
+        const ImageData = fs.readFileSync(imagePath);
+
+        fs.emptyDirSync(
+            join(TextureAtlasPath, "../", parse(TextureAtlas.$imagePath).name)
+        );
+
+        const TextureAtlasImage = await Jimp.read(ImageData);
+
+        for (const SubTexture of TextureAtlas.SubTexture) {
+            TextureAtlasImage.clone()
+                .crop(
+                    parseInt(SubTexture.$x),
+                    parseInt(SubTexture.$y),
+                    parseInt(SubTexture.$width),
+                    parseInt(SubTexture.$height)
+                )
+                .write(
+                    join(
+                        TextureAtlasPath,
+                        "../",
+                        parse(TextureAtlas.$imagePath).name,
+                        SubTexture.$name + parse(TextureAtlas.$imagePath).ext
+                    )
+                );
+        }
+    });
+
+program.parse();
